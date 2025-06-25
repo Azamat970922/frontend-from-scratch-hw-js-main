9ad7312d94ba0b123994d61cb010e764c2fcb75e
Azamat <petir97@mail.ru>
2025-06-25 19:36:36 +0500

    7

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -8,6 +8,15 @@ const score = 70 // тестовое значение, можно изменят
 let grade 
 // your code
 if (score >= 70 && score <= 79) {
-    grade = 'C'
-    console.log("Grade:" , grade);
-}
+    grade = 'C'   
+    } else if (score >= 0 && score <= 49) {
+        grade = 'F'
+    } else if (score >= 50 && score <= 69) {
+        grade = 'D'
+    } else if (score >= 80 && score <= 89) {
+        grade = 'B'
+    } else if (score >= 90 && score <= 100) {
+        grade = 'A'
+    }
+
+console.log('Grade',  grade)
